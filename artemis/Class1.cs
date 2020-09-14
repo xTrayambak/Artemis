e731e1ffa9de15d6cca9b90999fccfd75144c1a3
xTrayambak <59499552+xTrayambak@users.noreply.github.com>
2020-09-14 19:35:09 +0530

    Add files via upload

---
+++ artemis/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artemis
+{
+    public class calculationAPI
+    {
+        // addition
+        public int Add(int a, int b)
+        {
+            return a + b;
+
+        }
+
+        // subtraction
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+
+        }
+
+        // multiplication
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+
+        }
+
+        // division
+        public int Divide(int a, int b)
+        {
+            return a / b;
+
+        }
+    }
+}
